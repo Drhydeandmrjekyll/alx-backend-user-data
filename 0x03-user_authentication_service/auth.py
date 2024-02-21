dd213f54982d2722fe78921383f4230f23fc0613
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2024-02-21 15:17:06 +0000

    18_update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,31 +1,46 @@
-import uuid
+import bcrypt
 from db import DB
+from user import User
 
 
 class Auth:
+    """Auth class to interact with the authentication database."""
+
     def __init__(self):
         self._db = DB()
 
-    def get_reset_password_token(self, email):
-        """Generate a reset password token for user corresponding to the email.
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update the password of a user using the reset token.
 
         Args:
-            email (str): Email of the user.
-
-        Returns:
-            str: Reset password token.
+            reset_token (str): The reset token used to identify the user.
+            password (str): The new password to set for the user.
 
         Raises:
-            ValueError: If no user is found with provided email.
+            ValueError: If the reset token does not correspond to any user.
         """
-        user = self._db.find_user_by(email=email)
+        # Find user corresponding to reset token
+        user = self._db.find_user_by(reset_token=reset_token)
+
         if not user:
-            raise ValueError(f"No user found with email: {email}")
+            raise ValueError("Reset token is invalid")
 
-        # Generate UUID for reset password token
-        reset_token = str(uuid.uuid4())
+        # Hash new password
+        hashed_password = self._hash_password(password)
 
-        # Update user's reset_token database field
-        self._db.update_user_reset_token(user.id, reset_token)
+        # Update user's hashed password and reset_token fields
+        user.hashed_password = hashed_password
+        user.reset_token = None
 
-        return reset_token
+    def _hash_password(self, password: str) -> bytes:
+        """Hashes input password using bcrypt.
+
+        Args:
+            password (str): Password to hash.
+
+        Returns:
+            bytes: The hashed password.
+        """
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password
