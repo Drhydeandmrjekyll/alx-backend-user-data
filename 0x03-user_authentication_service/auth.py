8a5751a244097ddb6b206a2add9d741b1991dbaf
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2024-02-22 17:42:48 +0000

    5_register

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,10 @@
 Module to hash_password and interact with auth_DB
 """
 import bcrypt
+from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+
+from user import User
 
 
 def _hash_password(password: str) -> bytes:
@@ -20,5 +24,51 @@ def _hash_password(password: str) -> bytes:
     return hashed_pwd
 
 
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    """
+    def __init__(self) -> None:
+        """
+        Initialize Auth instance
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+
+        """
+        try:
+            existing_user = self._db.find_user_by(email=email)
+
+            if existing_user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            pass
+
+        hashed_pwd = _hash_password(password)
+
+        new_usr = self._db.add_user(email=email,
+                                    hashed_password=hashed_pwd.decode(
+                                        "utf-8"))
+        self._db
+        return new_usr
+
+
 if __name__ == '__main__':
-    print(_hash_password("password"))
+    email = 'me@me.com'
+    password = 'mySecuredPwd'
+
+    auth = Auth()
+
+    try:
+        user = auth.register_user(email, password)
+        print("successfully created a new user!")
+    except ValueError as err:
+        print("could not create a new user: {}".format(err))
+
+    try:
+        user = auth.register_user(email, password)
+        print("successfully created a new user!")
+    except ValueError as err:
+        print("could not create a new user: {}".format(err))
--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy import create_engine
 
 from user import Base, User
 
